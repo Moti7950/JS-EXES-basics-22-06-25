1b0f6c01d589a5fdd7d2b7fa2c3d873b0c6d3f5f
Moti7950 <shalom7950@gmail.com>
2025-06-24 23:48:09 +0300

    Q3

--- EXES-22-06-25.js
+++ EXES-22-06-25.js
@@ -8,4 +8,23 @@
 // b +=" "+ a
 // console.log(b.slice(lenib +1,lenib +1+ lenia));
 
- 
+//2
+// let grid = [
+//   ["X", "o", "X", "x", "o"],
+//   ["x", "X", "o", "o", "x"],
+//   ["o", "x", "X", "o", "o"],
+//   ["o", "o", "o", "x", "X"],
+//   ["X", "o", "o", "o", "x"]
+// ];
+
+// for (let row = 0; row < grid.length; row ++)
+// {
+//     for (let col = 0 ; col < grid[row].length; col++)
+//     {if (grid[row][col] === "X")
+//     {
+//       console.log(`Found X at row ${row}, col ${col}`)
+//     }
+
+//     }
+// }
+//3
