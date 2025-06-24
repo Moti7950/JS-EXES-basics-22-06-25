63762eae5069aac0580153e5a31d9543a8a94f71
Moti7950 <shalom7950@gmail.com>
2025-06-24 23:42:17 +0300

    Initial commit

---
+++ EXES-22-06-25.js
@@ -0,0 +1,11 @@
+//1
+// let a = "Moti"
+// let b = "Kopshitz"
+// const lenia = a.length
+// const lenib = b.length
+// a += " "+ b
+// console.log(a.slice(lenia +1));
+// b +=" "+ a
+// console.log(b.slice(lenib +1,lenib +1+ lenia));
+
+ 
